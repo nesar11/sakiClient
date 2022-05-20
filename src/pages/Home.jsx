05d353e90a3686d309bfc97060553721e6211727
nesar11 <nesar.uddin100@gmail.com>
2022-05-20 20:42:41 +0800

    slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 
+
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 function Home() {
   return (
     <div className='container'>
       <Announcement></Announcement>
         <Navbar></Navbar>
+        <Slider></Slider>
     </div>
   )
 }
